897982525f647dad74fc30e53c2eb4a6b11bd77d
Mukhamad Rozi <116157556+Roji21@users.noreply.github.com>
2024-07-07 15:06:37 +0700

    up

---
+++ app/src/main/java/com/rozi/gohits/model.kt
@@ -0,0 +1,20 @@
+package com.rozi.gohits
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+data class LoginResponse(
+    val token: String
+)
+
+data class RegisterRequest(
+    val username: String,
+    val password: String,
+    val email: String
+)
+
+data class RegisterResponse(
+    val message: String
+)
