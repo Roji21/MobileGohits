dc1c3ec25abdc56b2ff7dd5da44f96b664f220d2
Mukhamad Rozi <116157556+Roji21@users.noreply.github.com>
2024-07-19 09:37:04 +0700

    Add Event selesai

--- app/src/main/java/com/rozi/gohits/APIService.kt
+++ app/src/main/java/com/rozi/gohits/APIService.kt
@@ -1,12 +1,15 @@
 package com.rozi.gohits
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.Part
+
 
 interface ApiService {
     @FormUrlEncoded
@@ -29,4 +32,18 @@ interface ApiService {
     fun home(): Call<MenuHomeResponse>
     @GET("api/home")
     fun dash(): Call<MenuDasResponse>
+    @Multipart
+    @POST("api/addevent")
+    fun uploadImage(
+        @Part image: MultipartBody.Part,
+        @Part("iduser") iduser: RequestBody,
+        @Part("title") title: RequestBody,
+        @Part("date") date: RequestBody,
+        @Part("time") time: RequestBody,
+        @Part("price") price: RequestBody,
+        @Part("organizer") organizer: RequestBody,
+        @Part("location") location: RequestBody,
+        @Part("category") category: RequestBody,
+        @Part("participant") participant: RequestBody
+    ): Call<UploadResponse>
 }
